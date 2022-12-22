226c32572dd1f12183672b1c4a1fa237a7cdf795
Jaume Civera Soriano <jaumecvr2000@gmail.com>
2022-12-22 13:49:52 +0100

    partial type swagger

--- src/operation/dto/update-operation.dto.ts
+++ src/operation/dto/update-operation.dto.ts
@@ -1,4 +1,4 @@
 /* eslint-disable prettier/prettier */
-import { PartialType } from '@nestjs/mapped-types';
+import { PartialType } from '@nestjs/swagger';
 import { CreateOperationDto } from './create-operation.dto';
 export class UpdateOperationDto extends PartialType(CreateOperationDto) {}
